ecdf8a95e7da08d81e4c569380944d26a6ad0da8
nguyendinhlong04 <123631942+nguyendinhlong04@users.noreply.github.com>
2025-08-12 13:39:31 +0700

    Update japanpost.py

--- jp_redelivery/japanpost.py
+++ jp_redelivery/japanpost.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from caspio_api import SLOT_MAP
+from .caspio_api import SLOT_MAP
 
 def open_and_fill(driver: webdriver.Chrome,
                   link: str,
